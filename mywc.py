067c20239ba6490f655dfcd24777cc16f9c79ed3
Your Name <you@example.com>
2017-12-15 13:02:08 +0000

    Fixed bug with wrongly named var

--- mywc.py
+++ mywc.py
@@ -12,10 +12,10 @@ dd = {}
 with open(sys.argv[1]) as f:
     for s in f:
         for word in re.sub('\W', ' ', s).split():
-            d[word] = d.get(word, 0) + 1
+            dd[word] = dd.get(word, 0) + 1
             
 # Use list comprehension
-l = [(freq, word) for word, freq in d.items()]
+l = [(freq, word) for word, freq in dd.items()]
 
 for freq, word in sorted(l):
     print('%-6d %s' % (freq, word))
